a01dd1d5a79f495d65e7d120db69b3b69f54c949
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-03-20 15:01:50 -0400

    Initial Commit

---
+++ PrototypeScreens/PrototypeScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeScreensApp.swift
+//  PrototypeScreens
+//
+//  Created by Jorrel Tigbayan on 2025-03-20.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
